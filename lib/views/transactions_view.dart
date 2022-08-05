13ab09c0795ab2fdf98030ead54b2a968328da09
mananmehta3 <mananmehta@outlook.com>
2022-08-05 14:39:36 +0530

    Added Table to Transaction View

--- lib/views/transactions_view.dart
+++ lib/views/transactions_view.dart
@@ -5,6 +5,66 @@ class TransactionView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(title: const Text("Transactions")),
+      body: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+        child: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Table(
+              border: TableBorder.all(),
+              defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+              children: [
+                TableRow(children: [
+                  TableCell(
+                    child: Container(
+                        color: Colors.white,
+                        child: const Center(
+                            child: Text('Medicine',
+                                style: TextStyle(
+                                    fontSize: 20.0,
+                                    fontWeight: FontWeight.bold)))),
+                  ),
+                  TableCell(
+                    child: Container(
+                        color: Colors.white,
+                        child: const Center(
+                            child: Text('Quantity',
+                                style: TextStyle(
+                                    fontSize: 20.0,
+                                    fontWeight: FontWeight.bold)))),
+                  ),
+                  TableCell(
+                    child: Container(
+                        color: Colors.white,
+                        child: const Center(
+                            child: Text('Customer/Supplier',
+                                style: TextStyle(
+                                    fontSize: 20.0,
+                                    fontWeight: FontWeight.bold)))),
+                  ),
+                  TableCell(
+                    child: Container(
+                        color: Colors.white,
+                        child: const Center(
+                            child: Text('Amount',
+                                style: TextStyle(
+                                    fontSize: 20.0,
+                                    fontWeight: FontWeight.bold)))),
+                  ),
+                  TableCell(
+                    child: Container(
+                        color: Colors.white,
+                        child: const Center(
+                            child: Text('User',
+                                style: TextStyle(
+                                    fontSize: 20.0,
+                                    fontWeight: FontWeight.bold)))),
+                  )
+                ])
+              ]),
+        ),
+      ),
+    );
   }
 }
