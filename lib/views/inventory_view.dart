55b467775360dd8fe98c5fa5338a16f9713d16f3
mananmehta3 <mananmehta@outlook.com>
2022-08-06 19:43:14 +0530

    Added Code to Fetch Data from Medicine Table

--- lib/views/inventory_view.dart
+++ lib/views/inventory_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:learningdart/views/search_view.dart';
 
 class InventoryView extends StatefulWidget {
   late final String medicineName;
@@ -21,8 +23,67 @@ class _InventoryViewState extends State<InventoryView> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(title: Text('Inventory- $_medicineName')),
-        body: Column());
+    String qty = '';
+    String price = '';
+    final Stream<QuerySnapshot> medicineStream =
+        FirebaseFirestore.instance.collection('Medicines').snapshots();
+    return StreamBuilder<QuerySnapshot>(
+        stream: medicineStream,
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return Expanded(
+                  child: AlertDialog(
+                    title: const Text('Something went wrong'),
+                    actions: [
+                      TextButton(
+                        onPressed: () async {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const SearchView()),
+                          );
+                        },
+                        child: const Text('OK'),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            );
+          }
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          final List storeDocs = [];
+          snapshot.data!.docs.map((DocumentSnapshot document) {
+            Map a = document.data() as Map<String, dynamic>;
+            storeDocs.add(a);
+          }).toList();
+          for (var i = 0; i < storeDocs.length; i++) {
+            if (storeDocs[i]['Name'].toString() == _medicineName) {
+              qty = storeDocs[i]['Stock'].toString();
+              price = storeDocs[i]['Price'].toString();
+              break;
+            }
+          }
+          return Scaffold(
+              appBar: AppBar(title: Text('Inventory - $_medicineName')),
+              body: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Quantity - $qty',
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(
+                          fontSize: 30.0, fontWeight: FontWeight.bold)),
+                  Text('Price per pc - $price',
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(
+                          fontSize: 30.0, fontWeight: FontWeight.bold))
+                ],
+              ));
+        });
   }
 }
